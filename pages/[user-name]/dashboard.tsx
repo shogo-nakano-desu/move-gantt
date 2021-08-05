32282db4b25586d93e95c2af1d291589a037ba58
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-05 18:15:45 +0900

    create app bar

---
+++ pages/[user-name]/dashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MenuAppBar from "../../components/AppBar";
+
+const dashboard = () => {
+  return <MenuAppBar />;
+};
+export default dashboard;
---
+++ utils/dateGenerator.ts
@@ -0,0 +1,25 @@
+import { getYear, getMonth, getDate } from "date-fns";
+
+const dateGenerator = () => {
+  const today = new Date();
+  const year = "" + getYear(today);
+  let month: string | number = getMonth(today) + 1;
+  if (month < 10) {
+    month = "0" + month;
+  } else {
+    month = "" + month;
+  }
+  let date: string | number = getDate(today);
+  if (date < 10) {
+    date = "0" + date;
+  } else {
+    date = "" + date;
+  }
+  return {
+    year: year,
+    month: month,
+    date: date,
+  };
+};
+
+export default dateGenerator;
