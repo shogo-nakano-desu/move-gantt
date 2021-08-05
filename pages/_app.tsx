cfc18af2678c94e927348cf80ed29e3ffd8d6b47
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-05 09:59:30 +0900

    ログインのUIも完成した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,40 @@
-import React, { useEffect } from "react";
-import { AppProps } from "next/app";
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { StylesProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Theme from "../components/Theme";
 
-const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  useEffect(() => {
-    const jssStyles: Element | null =
-      document.querySelector("#jss-server-side");
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
+      jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
 
   return (
-    <StylesProvider injectFirst>
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
       <ThemeProvider theme={Theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </StylesProvider>
+    </React.Fragment>
   );
-};
+}
 
-export default CustomApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
