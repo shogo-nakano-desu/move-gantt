69dbfc0e98c94337525510433933c2868688004f
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-12 14:55:22 +0900

    modify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import SignIn from "./sign-in";
+import SignUp from "./sign-up";
 import Dashboard from "./[username]/dashboard";
 import { auth } from "../../firebase";
-import { Login, Logout } from "../utils/auth";
 
 const Home: React.VFC = () => {
   return <>{auth.currentUser ? <Dashboard /> : <SignIn />}</>;
