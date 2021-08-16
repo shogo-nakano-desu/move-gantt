051e719c9f4edae1e6f2fbb543572c8ba5c8fb06
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-16 17:22:35 +0900

    I will change grid to box. before that, commit

---
+++ src/components/AddTodoButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AddTaskIcon from "@material-ui/icons/AddTask";
+import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      margin: theme.spacing(1),
+    },
+  })
+);
+
+export default function AddTodoButtonComponent() {
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      className={classes.button}
+      startIcon={<AddTaskIcon />}
+    >
+      TODOを追加する
+    </Button>
+  );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,42 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/router";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { Container } from "@material-ui/core";
+import { sizing } from "@material-ui/system";
 
 import { setCurrentUser } from "../utils/reducers";
-import MenuAppBar from "../components/AppBar";
 import TodosComponent from "../components/Todos";
+import AddTodoButtonComponent from "../components/AddTodoButton";
 import { auth } from "../../firebase";
+import AppBarComponent from "../components/AppBar";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: { height: "100%", width: "100%" },
+    appbar: {
+      height: "7%",
+      width: "100%",
+    },
+    todos: {
+      height: "85%",
+      width: "100%",
+    },
+    addtodo: {
+      height: "10%",
+      width: "100%",
+    },
+    addtodocontainer: {
+      height: "100%",
+      width: "100%",
+    },
+  })
+);
 
 const Dashboard = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -18,16 +47,17 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <>
-      <>
-        <div>
-          <MenuAppBar />
-        </div>
-        <div>
-          <TodosComponent />
-        </div>
-      </>
-    </>
+    <Grid container>
+      <Grid className={classes.appbar} item>
+        <AppBarComponent />
+      </Grid>
+      <Grid className={classes.todos} item>
+        <TodosComponent />
+      </Grid>
+      <Grid className={classes.addtodo} container alignContent="space-between">
+        <AddTodoButtonComponent />
+      </Grid>
+    </Grid>
   );
 };
 export default Dashboard;
