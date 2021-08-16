0e9186b3cb16c578ed7e52058547473e5c27e81b
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-16 15:30:34 +0900

    create menu button

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,13 +1,36 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
+import { MenuItem, Menu, MenuItemProps } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+
+import { auth } from "../../firebase";
 import { dateGenerator } from "../utils/dateGenerator";
+
+type LinkMenuItemProps = Omit<
+  MenuItemProps<"a", { href: string }>,
+  "component" | "button"
+>;
+
+const LinkMenuItem = React.forwardRef<HTMLAnchorElement, LinkMenuItemProps>(
+  function LinkMenuItem(props, forwardedRef) {
+    const { href, ...other } = props;
+    return (
+      <Link href={href}>
+        <MenuItem component="a" button ref={forwardedRef} {...other} />
+      </Link>
+    );
+  }
+);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,7 +47,10 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function MenuAppBar() {
+  const router = useRouter();
   const classes = useStyles();
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
@@ -35,7 +61,23 @@ export default function MenuAppBar() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
 
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  // SignOut
+  const SignOut = async () => {
+    try {
+      await auth.signOut();
+      router.push("/sign-in");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   return (
     <div className={classes.root}>
       <AppBar className={classes.colorPrimary} position="static">
@@ -70,9 +112,33 @@ export default function MenuAppBar() {
               open={open}
               onClose={handleClose}
             >
-              <MenuItem onClick={handleClose}>
-                Profile(これクリックしたら直でユーザー画面にいくようにしたい)
-              </MenuItem>
+              <LinkMenuItem href="/profile">プロフィール</LinkMenuItem>
+              <LinkMenuItem href="/forms/form">
+                新規プロジェクト作成
+              </LinkMenuItem>
+              {/* モーダルを出して、ログアウトさせるようにしよう */}
+              <MenuItem onClick={handleDialogOpen}>ログアウト</MenuItem>
+              <Dialog
+                open={dialogOpen}
+                onClose={handleDialogClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {"ログアウトしてもよろしいでしょうか？"}
+                </DialogTitle>
+
+                <DialogActions>
+                  <Button onClick={handleDialogClose} color="primary">
+                    いいえ
+                  </Button>
+                  <form onSubmit={SignOut}>
+                    <Button type="submit" color="primary" autoFocus>
+                      はい
+                    </Button>
+                  </form>
+                </DialogActions>
+              </Dialog>
             </Menu>
           </div>
         </Toolbar>
--- src/utils/AuthProvider.tsx
+++ src/utils/AuthProvider.tsx
@@ -2,7 +2,7 @@ import { FC, createContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { auth } from "../../firebase";
-import { setCurrentUser, stateType, userType } from "./reducers";
+import { setCurrentUser, signOut, stateType, userType } from "./reducers";
 
 type AuthContextProps = {
   currentUser: userType | null | undefined;
@@ -20,6 +20,8 @@ const AuthProvider: FC = ({ children }) => {
         dispatch(setCurrentUser(user.uid, user.displayName));
       } else if (user) {
         dispatch(setCurrentUser(user.uid));
+      } else {
+        dispatch(signOut());
       }
     });
   }, []);
