8349ad923ff61eadb88e77fd2c1e2c2c69d134ce
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-18 14:38:19 +0900

    add test to utils. dateParser.ts & dateGenerator.ts

--- src/pages/sign-in.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import SignIn from "./sign-in";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<SignIn />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
