0328529d5c6c214aa4dcb6afc1f58f61be45447b
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-25 00:47:47 +0900

    fix some codes and test passes

--- src/utils/splitProcedures.test.ts
+++ src/utils/splitProcedures.test.ts
@@ -1,24 +1,188 @@
 import { splittedProcedures } from "./splitProcedures";
-import {
-  dummy_moveNotification,
-  dummy_car,
-  dummy_rentalCAN,
-  dummy_gasTapStop,
-} from "../info/procedures";
+
+const TARGET_PERSON = {
+  moveInTheSameMunicipalities: "moveInTheSameMunicipalities",
+  moveToDifferentMunicipalities: "moveToDifferentMunicipalities",
+  everyone: "everyone",
+} as const;
+type TARGET_PERSON = typeof TARGET_PERSON[keyof typeof TARGET_PERSON];
+
+interface procedureType {
+  id: string;
+  title: string;
+  startDate: number; // プロジェクト作成日か関数で計算した日付
+  deadline: number;
+  submitDestination: string;
+  targetPerson: TARGET_PERSON;
+  confirmationSource: string;
+  memo: string;
+  complete: boolean;
+  isNotEmployee: boolean;
+  isStudent: boolean;
+  isPet: boolean;
+  isScooter: boolean;
+  isCar: boolean;
+  isParking: boolean;
+  isUnderFifteen: boolean;
+  isFireInsurance: boolean;
+  isFixedPhone: boolean;
+  isMynumber: boolean;
+  isStampRegistration: boolean;
+  isDrivingLicense: boolean;
+  created_at: number;
+}
+
+const testProcedureA: procedureType = {
+  id: "testprocedurea",
+  title: "testA",
+  startDate: new Date(2021, 8, 1).getTime(),
+  deadline: new Date(2021, 8, 10).getTime(),
+  submitDestination: "DFIOAJFIOAJDIOEFJAO JEWOI ",
+  targetPerson: "everyone",
+  confirmationSource:
+    "https://hikkoshizamurai.jp/useful/procedure/other/credit-card/",
+  memo: "",
+  complete: false,
+  isNotEmployee: false,
+  isStudent: false,
+  isPet: false,
+  isScooter: false,
+  isCar: false,
+  isParking: false,
+  isUnderFifteen: false,
+  isFireInsurance: false,
+  isFixedPhone: false,
+  isMynumber: false,
+  isStampRegistration: false,
+  isDrivingLicense: false,
+  created_at: Date.now(),
+};
+
+const testProcedureB: procedureType = {
+  id: "testprocedureb",
+  title: "testB",
+  startDate: new Date(2021, 8, 1).getTime(),
+  deadline: new Date(2021, 8, 20).getTime(),
+  submitDestination: "DFIOAJFIOAJDIOEFJAO JEWOI ",
+  targetPerson: "everyone",
+  confirmationSource:
+    "https://hikkoshizamurai.jp/useful/procedure/other/credit-card/",
+  memo: "",
+  complete: false,
+  isNotEmployee: false,
+  isStudent: false,
+  isPet: false,
+  isScooter: false,
+  isCar: false,
+  isParking: false,
+  isUnderFifteen: false,
+  isFireInsurance: false,
+  isFixedPhone: false,
+  isMynumber: false,
+  isStampRegistration: false,
+  isDrivingLicense: false,
+  created_at: Date.now(),
+};
+
+const testProcedureC: procedureType = {
+  id: "testprocedurec",
+  title: "testC",
+  startDate: new Date(2021, 8, 2).getTime(),
+  deadline: new Date(2021, 8, 20).getTime(),
+  submitDestination: "DFIOAJFIOAJDIOEFJAO JEWOI ",
+  targetPerson: "everyone",
+  confirmationSource:
+    "https://hikkoshizamurai.jp/useful/procedure/other/credit-card/",
+  memo: "",
+  complete: false,
+  isNotEmployee: false,
+  isStudent: false,
+  isPet: false,
+  isScooter: false,
+  isCar: false,
+  isParking: false,
+  isUnderFifteen: false,
+  isFireInsurance: false,
+  isFixedPhone: false,
+  isMynumber: false,
+  isStampRegistration: false,
+  isDrivingLicense: false,
+  created_at: Date.now(),
+};
+
+const testProcedureD: procedureType = {
+  id: "testprocedured",
+  title: "testD",
+  startDate: new Date(2021, 6, 1).getTime(),
+  deadline: new Date(2021, 7, 1).getTime(),
+  submitDestination: "DFIOAJFIOAJDIOEFJAO JEWOI ",
+  targetPerson: "everyone",
+  confirmationSource:
+    "https://hikkoshizamurai.jp/useful/procedure/other/credit-card/",
+  memo: "",
+  complete: false,
+  isNotEmployee: false,
+  isStudent: false,
+  isPet: false,
+  isScooter: false,
+  isCar: false,
+  isParking: false,
+  isUnderFifteen: false,
+  isFireInsurance: false,
+  isFixedPhone: false,
+  isMynumber: false,
+  isStampRegistration: false,
+  isDrivingLicense: false,
+  created_at: Date.now(),
+};
+
+const testProcedureE: procedureType = {
+  id: "testproceduree",
+  title: "testE",
+  startDate: new Date(2021, 6, 1).getTime(),
+  deadline: new Date(2021, 10, 1).getTime(),
+  submitDestination: "DFIOAJFIOAJDIOEFJAO JEWOI ",
+  targetPerson: "everyone",
+  confirmationSource:
+    "https://hikkoshizamurai.jp/useful/procedure/other/credit-card/",
+  memo: "",
+  complete: false,
+  isNotEmployee: false,
+  isStudent: false,
+  isPet: false,
+  isScooter: false,
+  isCar: false,
+  isParking: false,
+  isUnderFifteen: false,
+  isFireInsurance: false,
+  isFixedPhone: false,
+  isMynumber: false,
+  isStampRegistration: false,
+  isDrivingLicense: false,
+  created_at: Date.now(),
+};
+
+const procedures = [
+  testProcedureA,
+  testProcedureB,
+  testProcedureC,
+  testProcedureD,
+  testProcedureE,
+];
+
+const spy = jest.spyOn(Date, "now");
+spy.mockReturnValue(1577804400000); // 2020/01/01
 
 test("split some procedures by week", () => {
-  expect(splittedProcedures()).toEqual({
-    weekOneProcedures: [dummy_rentalCAN], // 8/6から１週間
-    weekTwoProcedures: [], // 8/13-19
-    weekThreeProcedures: [], // 8/20-26
-    weekFourProcedures: [], // 8/27-9/2
-    weekFiveProcedures: [dummy_gasTapStop], // 9/3-9/9
-    weekSixProcedures: [dummy_moveNotification], //9/10-9/16
-    weekSevenProcedures: [dummy_car], //9/17-23
-    weekEightProcedures: [], //9/23-29
-  });
-  // 以下のテストは本番環境にあげるときに試せばOK
-  // expect(() => splittedProcedures()).toThrow(
-  //   "week8以降のTODOがあります。実装を確認しましょう！"
-  // );
+  expect(splittedProcedures(procedures, new Date(2021, 8, 10))).toEqual([
+    [testProcedureD],
+    [],
+    [],
+    [],
+    [testProcedureA],
+    [],
+    [testProcedureB, testProcedureC],
+    [testProcedureE],
+  ]);
+  spy.mockRestore();
 });
