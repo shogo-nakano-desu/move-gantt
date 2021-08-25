48c898720257bea241aee7927c9169fec0b19d4c
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-08-25 18:14:32 +0900

    fix dataGenerator.test.ts

--- src/utils/dateGenerator.test.ts
+++ src/utils/dateGenerator.test.ts
@@ -4,7 +4,7 @@ const dateToUse = new Date(2021, 8, 18) as unknown as string;
 jest.spyOn(global, "Date").mockImplementation(() => dateToUse);
 
 test("today described with string object", () => {
-  expect(dateGenerator()).toEqual({
+  expect(dateGenerator(1631890800000)).toEqual({
     year: "2021",
     month: "09",
     date: "18",
